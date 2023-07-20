9cd464eacd6a7ce4ebdd15a338dbd99bf8357578
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-20 18:35:29 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const {src, dest} = require('gulp');
 
 const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 
 function styles(){
-    return src('app/style/style.sass')
+    return src('app/style/style.css')
+    .pipe(concat('style.min.css'))
     .pipe(sass({ outputStyle: 'compressed'}))
     .pipe(dest('app/css'))
 }
