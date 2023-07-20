98737cae35e1c9a1c4ba2dac94555e030a92644d
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-20 18:45:29 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ const {src, dest} = require('gulp');
 
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
+const uglify =require('gulp-uglify-es').default
+
+function scripts(){
+    return src('app/js/main.js')
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+}
 
 function styles(){
     return src('app/style/style.css')
@@ -11,3 +19,4 @@ function styles(){
 }
 
 exports.styles = styles;
+exports.scripts = scripts;
